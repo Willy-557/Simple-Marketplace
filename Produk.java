d09247fe256002632b4111d0785c0db8bdb98fd7
Willy-557 <williamfsetiadi@gmail.com>
2026-01-02 18:33:24 +0700

    Make the logic of class Pembeli

---
+++ Produk.java
@@ -0,0 +1,62 @@
+class Produk {
+    String nama;
+    double harga;
+    int stok;
+
+    public Produk(String nama, double harga, int stok){
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    void cekInfo(){
+        System.out.println("Nama barang: " + this.nama);
+        System.out.println("Harga barang: " + this.harga);
+        System.out.println("Stock barang: " + this.stok);
+    }
+}
+
+class Penjual {
+    String nama;
+    double saldo;
+
+    Penjual(String nama, double saldo){
+        this.nama = nama;
+        this.saldo = saldo;
+    }
+
+    void cekDompet(){
+        System.out.println("Nama toko: " + this.nama);
+        System.out.println("Saldo toko: " + this.saldo);
+    }
+}
+
+class Pembeli {
+    String nama;
+    double saldo;
+
+    Pembeli(String nama, double saldo){
+        this.nama = nama;
+        this.saldo = saldo;
+    }
+
+    void beliBarang(Penjual siPenjual, Produk barangDibeli, int jumlahBeli){
+        double totalHarga = jumlahBeli * barangDibeli.harga;
+        if (jumlahBeli < barangDibeli.stok){
+            System.out.println("Stok Habis!");
+        }
+        else{
+            if (this.saldo < totalHarga) {
+                System.out.println("Uang tidak cukup!");
+            }
+            else{
+                double saldo = this.saldo - totalHarga;
+                double saldoPenjual = siPenjual.saldo + totalHarga;
+                int stokBarang = barangDibeli.stok - jumlahBeli;
+                System.out.println("Berhasil beli " + barangDibeli.nama + " , sisa uang: Rp " + this.saldo + " .");
+            }
+        }
+
+    }
+}
+
