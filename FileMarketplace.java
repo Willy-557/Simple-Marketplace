d363a8005757517f6f71ca02fef379faee2b88dd
Willy-557 <williamfsetiadi@gmail.com>
2026-01-02 20:24:18 +0700

    Make the logic of input the item on warehouse by seller

--- FileMarketplace.java
+++ FileMarketplace.java
@@ -88,6 +88,8 @@ public class FileMarketplace {
             }
             else {
                 if (opsi == 1) {
+                    
+
                     System.out.println("--- MARKETPLACE ---");
                     System.out.println("1. Lihat barang");
                     System.out.println("2. Beli barang");
@@ -131,13 +133,63 @@ public class FileMarketplace {
                                     Produk pembelian = katalogProduk.get(noBarang);
                                     System.out.print("Mau beli berapa unit: ");
                                     int jumlahPembelianStok = scanner.nextInt();
-                                    saya.beliBarang(toko, pembelian, jumlahPembelianStok);
+                                    pembeliBaru.beliBarang(toko, pembelian, jumlahPembelianStok);
                                 }
                         }
                     }
                 }
+
+                else if (opsi == 2) {
+                    while (true) {
+                        System.out.println("--- Gudang ---");
+                        System.out.println("1. Tambah Barang");
+                        System.out.println("2. Keluar");
+                        System.out.print(">> ");
+                        int choiceSeller = scanner.nextInt();
+
+                        if (choiceSeller == 2){
+                            break;
+                        }
+                        else if (choiceSeller > 2 || choiceSeller < 0){
+                            System.out.println("Harap masukkan menu 1 sampai 2!");
+                            continue;
+                        }
+                        else if (choiceSeller == 1) {
+
+                            System.out.println("\n=== Daftar produk ===\n");
+                            for (Integer nomor : katalogProduk.keySet()){
+                                Produk barang = katalogProduk.get(nomor);
+                                System.out.println(nomor + ". Nama barang: " + barang.nama + "\n" + "   Harga barang: " + barang.harga + "\n" + "   Stok barang: " + barang.stok);
+                                System.out.println("-------------------------------");
+                                System.out.println("\n");
+                            }
+                            System.out.println("Masukkan index barang berdasarkan barang yang terakhir: ");
+                            int idxBarang = scanner.nextInt();
+
+                            scanner.nextLine();
+                            System.out.print("Masukkan nama barang: ");
+                            String namaBarang = scanner.nextLine();
+                            
+                            System.out.print("Masukkan harga barang: ");
+                            double hargaBarang = scanner.nextInt();
+
+                            System.out.print("Masukkan stock barang: ");
+                            int stockBarang = scanner.nextInt();
+
+                            Produk produkBaru = new Produk(namaBarang, hargaBarang, stockBarang);
+                            katalogProduk.put(idxBarang, produkBaru);
+
+                            System.out.println("\nBerhasil menambahkan barang dengan ketentuan: ");
+                            System.out.println("Nama barang: " + namaBarang);
+                            System.out.println("Harga barang: " + hargaBarang);
+                            System.out.println("Stock barang: " + stockBarang);
+
+
+                        }
+                    }
+                    }
+                    
+                }
             }
         }
     }
-}
-
